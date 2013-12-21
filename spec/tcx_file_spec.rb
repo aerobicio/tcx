c0f5e8ccb86c83bc8a2de2cfa69fdc7a7d5cbd71
Gareth Townsend <gareth.townsend@me.com>
2013-12-21 17:33:15 +1100

    Parse TCX file, making basic pending tests pass.

--- spec/tcx_file_spec.rb
+++ spec/tcx_file_spec.rb
@@ -7,24 +7,24 @@ describe Tcx::TcxFile do
   describe "#active_duration" do
     subject { tcx_file.active_duration }
 
-    pending
+    it { should == 2325.0200000 }
   end
 
   describe "#distance" do
     subject { tcx_file.distance }
 
-    pending
-  end
-
-  describe "#end_time" do
-    subject { tcx_file.end_time }
-
-    pending
+    it { should == 8348.5039063 }
   end
 
   describe "#start_time" do
     subject { tcx_file.start_time }
 
-    pending
+    it { should == Time.parse("2007-08-07T02:42:41Z") }
+  end
+
+  describe "#end_time" do
+    subject { tcx_file.end_time }
+
+    it { should == Time.parse("2007-08-07T02:42:41Z") + BigDecimal.new("2325.0200000") }
   end
 end
